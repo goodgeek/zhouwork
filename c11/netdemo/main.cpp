309349377520fc033ebf8ecaa2044546b8228604
root <root@localhost.localdomain>
2016-10-19 01:35:17 -0400

    c11 move

--- c11/netdemo/main.cpp
+++ c11/netdemo/main.cpp
@@ -7,10 +7,17 @@
 #include "threadnet.h"
 #include <array>
 #include "sealbase.h"
+#include <iostream>
 
 int main()
 {
-    SealBase base(20, "zhou");
+    int i = 20;
+    int &j = i;
+    int &&k = std::move(i);
 
+    std::cout << "i=" << i << std::endl; 
+    std::cout << "j=" << j << std::endl;
+    std::cout << "k=" << k << std::endl;
+    std::cout << "i=" << i << std::endl; 
     return 0;
 }
