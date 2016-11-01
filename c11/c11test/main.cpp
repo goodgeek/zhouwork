d311e48c6ff70e9fc3f25ce91da98883f47c7181
zhoujinlu <zhoujinlugeek@gmail.com>
2016-11-01 01:21:22 -0400

    enum class

--- c11/c11test/main.cpp
+++ c11/c11test/main.cpp
@@ -6,8 +6,11 @@
 class TestBase
 {
 public:
+    enum Result { NoError, Success };
+
     TestBase(int age): age_(age) {}
     std::string showMessage() { return "hello"; }
+    Result gotoZero() { return Success; }
 private:
     int age_;
 };
@@ -17,6 +20,7 @@ int main(int argc, char *argv[])
     std::shared_ptr<TestBase> ptrTestBase = std::make_shared<TestBase>(5);
     std::cout << "ptr count:" <<  ptrTestBase.use_count() << std::endl;
     std::cout << ptrTestBase.get()->showMessage() << std::endl;
+    std::cout << ptrTestBase.get()->gotoZero() << std::endl;
 
     return 0;
 }
