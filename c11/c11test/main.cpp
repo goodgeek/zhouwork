b552fc5184b7228218417ba31af6cd6163819db1
zhoujinlu <zhoujinlugeek@gmail.com>
2016-11-15 03:36:53 -0500

    tuple test

--- c11/c11test/main.cpp
+++ c11/c11test/main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <vector>
 #include <iostream>
+#include <tuple>
 
 class Object
 {
@@ -44,8 +45,34 @@ int compare(const T &a, const T &b)
     if (b < a) return 1; 
 } 
 
+template <typename T>
+class Blob {
+public:
+    typedef T value_type;
+    typedef typename std::vector<T>::size_type size_type;
+
+    Blob();
+    Blob(std::initializer_list<T> il);
+
+    size_type size() const { return data->size(); }
+    bool empty() const { return data->empty(); }
+    void push_back(const T &t) { data->push_back(t); }
+
+    void push_back(T &&t) { data->push_back(std::move(t)); }
+    void pop_back();
+
+    T& back();
+    T& operator[] (size_type i);
+
+private:
+    std::shared_ptr<std::vector<T>> data;
+    void check(size_type i, const std::string &msg) const;
+};
+
+
 int main(int argc, char *argv[])
 {
-    compare(5, 6);
+    std::tuple<int, int, int> ret(1, 5, 9);
+    std::cout << std::get<0>(ret) << std::endl;
     return 0;
 }
