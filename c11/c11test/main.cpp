40ee01f73724fbe795afc7151f4ed571438237c0
zhoujinlu <zhoujinlugeek@gmail.com>
2016-11-23 03:30:55 -0500

    container

--- c11/c11test/main.cpp
+++ c11/c11test/main.cpp
@@ -21,6 +21,7 @@
 #include <time.h>
 #include <thread>
 #include <algorithm>
+#include <sys/time.h>
 
 using std::string;
 using std::cout;
@@ -33,16 +34,47 @@ public:
     void ShowPrint() { return; }
 };
 
+void showMessage()
+{
+    cout << "hello world" << endl;
+}
+
 int main(int argc, char *argv[])
 {
-    std::vector<string> strvec = {"hello", "what", "is", "your", "name"};
-    std::sort(strvec.begin(), strvec.end());
+    std::map<int, string> vec;
 
-    std::vector<string> str2vec(5);
+    std::uniform_int_distribution<unsigned> u(100, 1000000);
+    std::default_random_engine e(time(NULL));
 
-    for (auto i : strvec) {
+    struct timeval startTime;
+
+    gettimeofday(&startTime, NULL);
+
+    for (int i = 0; i < 10000000; i++) {
+        vec.insert({i, std::to_string(i)});
+        //vec.push_back(u(e)); 
+        //vec.insert_after(vec.end(), i);
+        //vec.push_front(u(e));
+        //vec.push_back(u(e)); 
+    }
+
+
+    cout << "Insert data ok" << endl;
+    //std::sort(vec.begin(), vec.end());
+    cout << "compleate " << endl;
+
+    /*
+    for (int i = 0; i < 100; i++) {
         cout << i << endl;
     }
+    */
+
+    struct timeval endTime;
+    gettimeofday(&endTime, NULL);
+
+    long sec = (endTime.tv_sec - startTime.tv_sec) * 1000 + (endTime.tv_usec - startTime.tv_usec) % 1000;
+    cout << "msec: " << sec << endl;
+
     return 0;
 }
 
