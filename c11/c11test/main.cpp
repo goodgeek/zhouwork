c015ec7adab4d3a23c69617efa1c73728edd8f1d
zhoujinlu <zhoujinlugeek@gmail.com>
2016-12-11 22:01:56 -0500

    operator +

--- c11/c11test/main.cpp
+++ c11/c11test/main.cpp
@@ -1,29 +1,29 @@
 #include <iostream>
 #include "base.h"
-#include "human.h"
 #include <vector>
 #include <memory>
+#include <map>
 
 using std::string;
 using std::cout;
 using std::cin;
 using std::endl;
 
+class Human {
+public:
+    int getNumber() const { return i_; }
+    int setNumbar(int i) { i_ = i; }
+    int operator() (int a) { return a + i_; }
+    operator int() { return 20; }
+private:
+    int i_;
+};
+
 int main(int argc, char *argv[])
 {
-    std::shared_ptr<Base> ptrBase = 
-        std::make_shared<Base>("zhou", 30, new Human("j"));
-
-    Base *ptrBase2 = new Base("jj", 20, new Human("jjj"));
-    ptrBase.reset(ptrBase2);
-    string name = ptrBase->getName();
-    cout << "Name: " << name << endl; 
-
-    string line;
-    while (cin >> line) {
-        if (line == "quit") 
-            break;
-    }
+    Human h;
+    Human s;
+    cout << s + "" << endl; 
 
     return 0;
 }
