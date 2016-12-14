37d955c1b48d0b184a5f9a4e7b4ef3421ab64b71
zhoujinlu <zhoujinlugeek@gmail.com>
2016-12-14 03:49:34 -0500

    template

--- c11/c11test/main.cpp
+++ c11/c11test/main.cpp
@@ -11,6 +11,10 @@ using std::endl;
 
 class Human {
 public:
+    Human() = default;
+    Human(int i) : i_(i) {}
+    Human(const Human &h) : i_(h.i_) {}
+    Human& operator=(const Human &h) { i_ = h.i_; return *this; }
     int getNumber() const { return i_; }
     int setNumbar(int i) { i_ = i; }
     int operator() (int a) { return a + i_; }
@@ -19,11 +23,25 @@ private:
     int i_;
 };
 
+class Ch : public Human {
+public:
+    Ch(const Ch &h) : j_(h.j_) {}
+    Ch& operator=(const Ch &h) { j_ = h.j_; return *this; }
+    string showMessage() { return "message"; } 
+private:
+    int j_;
+};
+
+template<typename T>
+int add(const T &t, const T &a)
+{
+    return a * t;
+}
+
 int main(int argc, char *argv[])
 {
-    Human h;
-    Human s;
-    cout << s + "" << endl; 
+    Ch h;
+    Ch h2;
 
     return 0;
 }
--- python/1.py
+++ python/1.py
@@ -1,4 +1,15 @@
 #! /usr/bin/python
 
-for i in range(10):
-    print(i)
+import threading
+import time
+
+lock = threading.Lock()
+
+def showMessage(name):
+    lock.acquire()
+    print("hello thread", threading.currentThread().name)
+    lock.release()
+
+t1 = threading.Thread(target=showMessage, args=("hello",))
+t1.start()
+t1.join()
